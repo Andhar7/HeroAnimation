9f830129f5ec9767b931328ab0b2a4780e59db36
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-22 16:07:04 +0100

    Initial Commit

---
+++ HeroAnimation/HeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationApp.swift
+//  HeroAnimation
+//
+//  Created by Andrej Kling on 22.12.20.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
